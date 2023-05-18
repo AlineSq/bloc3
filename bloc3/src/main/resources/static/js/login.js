407bd4b33036c7f1e6303b44d0ca3724cb05d7e2
AlineSq <joytoday@hotmail.fr>
2023-05-18 21:32:56 +0200

    Gestion de la visibilité du mot de passe

--- bloc3/src/main/resources/static/js/login.js
+++ bloc3/src/main/resources/static/js/login.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
 
+    let passwordInput = $("#idPassword");
+
     $("#idFormLogin").submit(function(event) {
         event.preventDefault(); // Empêcher le formulaire de soumettre les données via POST
 
         // Récupérer les valeurs saisies dans le formulaire
         let email = $("#idEmail").val();
-        let password = $("#idPassword").val();
+        let password = passwordInput.val();
 
         loginQuery(
             email,
             password,
             () => { window.location.href = "/catalog-admin"; }
         );
+    });
+
+    $("#idButtonChangePasswordVisility").on("click", (event) => {
+        event.preventDefault();
+
+        if (passwordInput.attr("type") == "password")
+            passwordInput.attr("type","text");
+        else
+            passwordInput.attr("type","password");
     });
 });
--- bloc3api/src/main/java/com/studi/bloc3api/services/ProductService.java
+++ bloc3api/src/main/java/com/studi/bloc3api/services/ProductService.java
@@ -3,7 +3,6 @@ package com.studi.bloc3api.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Service;
 
 import com.studi.bloc3api.models.Product;
